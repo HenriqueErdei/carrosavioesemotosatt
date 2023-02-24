269d637a2571d9f539459db18ea95a011a226cc9
Henrique Erdei <henriqueerdeijob@gmail.com>
2023-02-24 09:51:50 -0300

    att

--- src/veiculos/laviao.ts
+++ src/veiculos/laviao.ts
@@ -2,6 +2,9 @@ import { aviao } from "../Interfaces/Iaviao";
 
 export const respostaaviao: aviao <string> [] = [
     {Modelo: "Aero", Cor: "Branco", Ano: 2021, Placa: "ABCD-1444", Obs: "Detalhes em Couro Caramelo",},
-    {Modelo: "Jato", Cor: "Amarelo", Ano: 2020, Placa: "YUOX-2525", Obs: "Detalhes na lateral",},
+    {Modelo: "Jato", Cor: "Preto", Ano: 2020, Placa: "YUOX-2525", Obs: "Detalhes na lateral",},
+    {Modelo: "Jato Vaper", Cor: "Branco", Ano: 2020, Placa: "ZZOX-2525", Obs: "Detalhes azul",},
+    {Modelo: "Caça Nx", Cor: " Preto", Ano: 2021, Placa: "HHXZ-9090", Obs: "Misseis TLG",},
+    
 ]
 
